deac249f0ca862ca624ce6ffa44cc452ee4d4063
Eros <eros.burelli@bvtech.com>
2026-03-17 11:34:55 +0100

    First commit

---
+++ src/test/java/com/ofp/apigateway/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ofp.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
